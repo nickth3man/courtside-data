7f52e8440aaea1b45ee56b69e1bc4b6b6fde8ce8
Jae Bradley <jae.b.bradley@gmail.com>
2024-12-06 18:02:42 -0500

    Re-enable Season Schedule Integration Tests (#297)

--- tests/integration/client/test_season_schedule.py
+++ tests/integration/client/test_season_schedule.py
@@ -1,20 +1,78 @@
+import filecmp
+import functools
 import json
 import os
-from datetime import date, datetime
-from pathlib import Path
+from datetime import datetime
 from unittest import TestCase
 
 import pytz
+import requests_mock
 
 from basketball_reference_web_scraper.client import season_schedule
 from basketball_reference_web_scraper.data import OutputType, Team
-from basketball_reference_web_scraper.errors import InvalidSeason
 
 
+class SeasonScheduleMocker:
+    def __init__(self, schedules_directory, season_end_year):
+        self._schedules_directory = schedules_directory
+        self._season_end_year = season_end_year
+
+    def decorate_class(self, klass):
+        for attr_name in dir(klass):
+            if not attr_name.startswith('test_'):
+                continue
+
+            attr = getattr(klass, attr_name)
+            if not hasattr(attr, '__call__'):
+                continue
+
+            setattr(klass, attr_name, self.mock(attr))
+
+        return klass
+
+    def mock(self, callable):
+        @functools.wraps(callable)
+        def inner(*args, **kwargs):
+            html_files_directory = os.path.join(self._schedules_directory, str(self._season_end_year))
+            self.responses_by_url = {}
+            for file in os.listdir(os.fsencode(html_files_directory)):
+                filename = os.fsdecode(file)
+                if not filename.endswith(".html"):
+                    raise ValueError(
+                        f"Unexpected prefix for {filename}. Expected all files in {html_files_directory} to end with .html.")
+
+                with open(os.path.join(html_files_directory, filename), 'r') as file_input:
+                    if filename.startswith(str(self._season_end_year)):
+                        key = f"https://www.basketball-reference.com/leagues/NBA_{self._season_end_year}_games.html"
+                    else:
+                        key = f"https://www.basketball-reference.com/leagues/NBA_{self._season_end_year}_games-{filename}"
+                    self.responses_by_url[key] = file_input.read()
+            with requests_mock.Mocker() as m:
+                for url, response in self.responses_by_url.items():
+                    m.get(url, text=response, status_code=200)
+                return callable(*args, **kwargs)
+
+        return inner
+
+    def __call__(self, obj):
+        if isinstance(obj, type):
+            return self.decorate_class(obj)
+
+        raise ValueError("Should only be used as a class decorator")
+
+
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2018
+)
 class TestSeasonScheduleInMemoryOutput(TestCase):
+
     def test_2018_season_schedule_length(self):
         result = season_schedule(season_end_year=2018)
-        self.assertEqual(1312, len(result))
+        self.assertEqual(1416, len(result))
 
     def test_first_game_of_2018_season(self):
         result = season_schedule(season_end_year=2018)
@@ -32,7 +90,7 @@ class TestSeasonScheduleInMemoryOutput(TestCase):
     def test_last_game_of_2018_season(self):
         result = season_schedule(season_end_year=2018)
         self.assertEqual(
-            result[1311],
+            result[1415],
             {
                 "away_team": Team.GOLDEN_STATE_WARRIORS,
                 "away_team_score": 108,
@@ -42,111 +100,178 @@ class TestSeasonScheduleInMemoryOutput(TestCase):
             }
         )
 
-    def test_future_season_schedule_throws_invalid_season_error(self):
-        current_year = date.today().year
-        future_year = current_year + 10
-        expected_message = "Season end year of {future_year} is invalid".format(future_year=future_year)
-        self.assertRaisesRegex(
-            InvalidSeason,
-            expected_message,
-            season_schedule,
-            season_end_year=future_year
-        )
 
-
-class TestSeasonScheduleCSVOutput(TestCase):
+class TestFutureSeasonSchedule(TestCase):
     def setUp(self):
-        self.output_2018_file_path = os.path.join(
+        with open(os.path.join(
+                os.path.dirname(__file__),
+                f"../files/schedule/not-found.html",
+        ), 'r') as file_input: self._html = file_input.read()
+
+    @requests_mock.Mocker()
+    def test_future_season_schedule_returns_empty_list(self, m):
+        m.get(url=f"https://www.basketball-reference.com/leagues/NBA_2026_games.html", text=self._html, status_code=200)
+        result = season_schedule(season_end_year=2026)
+        self.assertEqual([], result)
+
+
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2018
+)
+class Test2018SeasonScheduleCsvOutput(TestCase):
+    def setUp(self):
+        self.output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/2018_season_schedule.csv"
+            "./output/generated/season_schedule/2018.csv"
         )
-        self.expected_output_2018_file_path = os.path.join(
+        self.expected_output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/expected/2018_season_schedule.csv"
-        )
-        self.output_2001_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "../output/2001_season_schedule.csv"
-        )
-        self.expected_output_2001_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "../output/expected/2001_season_schedule.csv"
+            "./output/expected/season_schedule/2018.csv"
         )
 
     def tearDown(self):
-        if Path(self.output_2018_file_path).exists():
-            os.remove(self.output_2018_file_path)
+        os.remove(self.output_file_path)
 
-        if Path(self.output_2001_file_path).exists():
-            os.remove(self.output_2001_file_path)
-
-    def test_2018_season_schedule_csv(self):
-        season_schedule(season_end_year=2018, output_type=OutputType.CSV, output_file_path=self.output_2018_file_path)
-        with open(self.output_2018_file_path, "r", encoding="utf8") as output_file, \
-                open(self.expected_output_2018_file_path, "r", encoding="utf8") as expected_output_file:
-            self.assertEqual(
-                output_file.readlines(),
-                expected_output_file.readlines()
-            )
-
-    def test_2001_season_schedule_csv(self):
-        season_schedule(season_end_year=2001, output_type=OutputType.CSV, output_file_path=self.output_2001_file_path)
-        with open(self.output_2001_file_path, "r", encoding="utf8") as output_file, \
-                open(self.expected_output_2001_file_path, "r", encoding="utf8") as expected_output_file:
-            self.assertEqual(
-                output_file.readlines(),
-                expected_output_file.readlines()
-            )
+    def test_output(self):
+        season_schedule(season_end_year=2018, output_type=OutputType.CSV, output_file_path=self.output_file_path)
+        self.assertTrue(
+            filecmp.cmp(
+                self.output_file_path,
+                self.expected_output_file_path))
 
 
-class TestSeasonScheduleJSONOutput(TestCase):
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2018
+)
+class Test2018SeasonScheduleJsonOutput(TestCase):
     def setUp(self):
-        self.output_2018_file_path = os.path.join(
+        self.output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/2018_season_schedule.json"
+            "./output/generated/season_schedule/2018.json"
         )
-        self.expected_output_2018_file_path = os.path.join(
+        self.expected_output_file_path = os.path.join(
             os.path.dirname(__file__),
-            "../output/expected/2018_season_schedule.json"
-        )
-        self.output_2001_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "../output/2001_season_schedule.json"
-        )
-        self.expected_output_2001_file_path = os.path.join(
-            os.path.dirname(__file__),
-            "../output/expected/2001_season_schedule.json"
+            "./output/expected/season_schedule/2018.json"
         )
 
     def tearDown(self):
-        if Path(self.output_2018_file_path).exists():
-            os.remove(self.output_2018_file_path)
+        os.remove(self.output_file_path)
 
-        if Path(self.output_2001_file_path).exists():
-            os.remove(self.output_2001_file_path)
+    def test_file_output(self):
+        season_schedule(season_end_year=2018, output_type=OutputType.JSON, output_file_path=self.output_file_path)
+        self.assertTrue(
+            filecmp.cmp(
+                self.output_file_path,
+                self.expected_output_file_path))
 
-    def test_2018_season_schedule_in_memory_json(self):
-        result = season_schedule(season_end_year=2018, output_type=OutputType.JSON)
-        with open(self.expected_output_2018_file_path, "r", encoding="utf8") as expected_output_file:
+
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2018
+)
+class Test2018SeasonScheduleInMemoryJson(TestCase):
+    def setUp(self):
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/season_schedule/2018.json"
+        )
+
+    def test_in_memory_json(self):
+        schedule = season_schedule(season_end_year=2018, output_type=OutputType.JSON)
+        with open(self.expected_output_file_path, "r", encoding="utf8") as f:
             self.assertEqual(
-                json.loads(result),
-                json.load(expected_output_file)
+                json.load(f),
+                json.loads(schedule),
             )
 
-    def test_writing_2018_season_schedule_json_file(self):
-        season_schedule(season_end_year=2018, output_type=OutputType.JSON, output_file_path=self.output_2018_file_path)
-        with open(self.output_2018_file_path, "r", encoding="utf8") as output_file, \
-                open(self.expected_output_2018_file_path, "r", encoding="utf8") as expected_output_file:
-            self.assertEqual(
-                json.load(output_file),
-                json.load(expected_output_file),
-            )
 
-    def test_writing_2001_season_schedule_json_file(self):
-        season_schedule(season_end_year=2001, output_type=OutputType.JSON, output_file_path=self.output_2001_file_path)
-        with open(self.output_2001_file_path, "r", encoding="utf8") as output_file, \
-                open(self.expected_output_2001_file_path, "r", encoding="utf8") as expected_output_file:
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2001
+)
+class Test2001SeasonScheduleCsvOutput(TestCase):
+    def setUp(self):
+        self.output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/generated/season_schedule/2001.csv"
+        )
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/season_schedule/2001.csv"
+        )
+
+    def tearDown(self):
+        os.remove(self.output_file_path)
+
+    def test_output(self):
+        season_schedule(season_end_year=2001, output_type=OutputType.CSV, output_file_path=self.output_file_path)
+        self.assertTrue(
+            filecmp.cmp(
+                self.output_file_path,
+                self.expected_output_file_path))
+
+
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2001
+)
+class Test2018SeasonScheduleJsonOutput(TestCase):
+    def setUp(self):
+        self.output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/generated/season_schedule/2001.json"
+        )
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/season_schedule/2001.json"
+        )
+
+    def tearDown(self):
+        os.remove(self.output_file_path)
+
+    def test_file_output(self):
+        season_schedule(season_end_year=2001, output_type=OutputType.JSON, output_file_path=self.output_file_path)
+        self.assertTrue(
+            filecmp.cmp(
+                self.output_file_path,
+                self.expected_output_file_path))
+
+
+@SeasonScheduleMocker(
+    schedules_directory=os.path.join(
+        os.path.dirname(__file__),
+        "../files/schedule",
+    ),
+    season_end_year=2001
+)
+class Test2018SeasonScheduleInMemoryJson(TestCase):
+    def setUp(self):
+        self.expected_output_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "./output/expected/season_schedule/2001.json"
+        )
+
+    def test_in_memory_json(self):
+        schedule = season_schedule(season_end_year=2001, output_type=OutputType.JSON)
+        with open(self.expected_output_file_path, "r", encoding="utf8") as f:
             self.assertEqual(
-                json.load(output_file),
-                json.load(expected_output_file),
+                json.load(f),
+                json.loads(schedule),
             )
